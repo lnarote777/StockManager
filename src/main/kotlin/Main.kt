8130c53a92dbe51f5873cfd767d54e5225a8c3dd
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-10-31 21:18:36 +0100

    cambiar el login

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 
+import jakarta.persistence.EntityManagerFactory
+import jakarta.persistence.Persistence
+import org.example.model.Usuario
 import org.example.output.Console
 import org.example.repository.RepoProduct
 import org.example.repository.RepoProveedor
@@ -8,6 +11,21 @@ import org.example.service.ProductService
 import org.example.service.ProveedorService
 
 fun main() {
+
+//    val emf: EntityManagerFactory = Persistence.createEntityManagerFactory("StockManager")
+//    val em = emf.createEntityManager()
+//
+//    em.transaction.begin()
+//
+//    val u = Usuario("1234", "larux")
+//
+//    //dp de hacer el persist, ya estaria en el persistence context
+//    em.persist(u)
+//
+//    //Ya estaria enn la bd
+//    em.transaction.commit()
+//    em.close()
+
     //Instancias repository
     val productRepo = RepoProduct()
     val proveedorRepo = RepoProveedor()
@@ -22,4 +40,5 @@ fun main() {
     val menu = Menu(consola, proveedorService, productService, userRepo)
     menu.menuLogin()
     menu.menuGestion()
+
 }
